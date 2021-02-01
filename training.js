f2c6ba26a9931f055afed9cf076e551e9ea05f4f
LiseBrun <74918580+LiseBrun@users.noreply.github.com>
2021-02-01 17:04:26 +0100

    Update training.js

--- training.js
+++ training.js
@@ -4,49 +4,126 @@
 var materials = {
 		'mirror' : [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
 		'file_names' : [
-			  "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T0e.png",
-			  "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T0h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T1e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T1h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T2e.png",
+			  "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s0e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T0h.png",
+			  "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T1e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T1h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T2e.png",
 				//5
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T2h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T3e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T3h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T4e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T4h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T2h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p1e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p1h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p2e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p2h.png",
 				//10
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T5e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T5h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T6e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T6h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T7e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r5e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r5h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r2e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r2h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q4e.png",
 				//15
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T7h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T8e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T8h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T9e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T9h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q4h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q1e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q1h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s15e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s15h.png",
 				//20
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T01e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T01h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T02e.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T02h.png",
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T10e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s30e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s30h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T3e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T3h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T4e.png",
 				//25
-				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T10h.png"
-				// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T0e60.png",
-				// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T0e80.png",
-				// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/T0e100.png",
-				// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/ligne.png",
-				// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/github/ligne10.png"
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T4h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p3e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p3h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p4e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/p4h.png",
+				//30
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r1e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r1h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r3e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r3h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q2e.png",
+				//35
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q2h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q3e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q3h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s45e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s45h.png",
+				//40
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s75e.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s75h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s0h.png",
+				"https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T110e.png",
+
 			],
 
-		'xstarts' : [32, 32, 35, 35, 95, 33, 317, 139, 278, 149, 284, 361, 359, 360, 305, 305, 67, 198, 52, 52, 5, 5, 311, 311, 35, 35],
-		'ystarts' : [59, 59, 44, 92, 53, 113, 247, 31, 28, 256, 42, 55, 213, 238, 261, 261, 229, 276, 264, 264, 138, 138, 81, 81, 28, 172],
-		'xends' :   [298, 298, 333, 333, 245, 183, 228, 218, 179, 247, 88, 164, 71, 71, 47, 47, 187, 317, 310, 310, 327, 327, 20, 20, 249, 249],
-		'yends' :   [243, 243, 213, 262, 172, 231, 143, 134, 255, 28, 218, 231, 154, 178, 10, 10, 83, 130, 14, 14, 163, 163, 220, 220, 66, 211]
+		'xstarts' : [56, 32, 35, 333, 95, 245, 45, 383, 135, 13, 290, 46, 16,	210, 92, 158, 51, 360, 325, 41, 55, 309, 317, 139, 278, 179, 303, 351, 39, 10, 16, 326, 109, 218, 36, 41, 228, 228, 135, 342, 202, 126, 56, 208],
+		'ystarts' : [146, 59, 44, 94, 53, 113, 184, 258, 19, 40, 41, 183, 41, 285, 221,	283, 253, 105, 8, 139, 139, 46, 45, 247, 31, 28, 28, 244, 128, 120, 20, 276, 100, 251, 250, 69, 41, 218, 37, 215, 215, 286, 286, 146, 20],
+		'xends' :   [352, 298, 333, 35, 245, 95, 383, 45, 13, 135, 290, 46, 290, 210, 16, 158, 92, 41, 325, 309, 55, 360, 51, 228, 218, 179, 278, 351, 303, 10, 39, 326, 16, 218, 109, 41, 36, 28, 28, 342, 135,126, 203, 352, 3],
+		'yends' :   [146, 243, 213, 262, 172, 232, 11, 86, 253, 275, 28, 169, 28, 72, 8, 26, 26, 152, 254, 63, 63, 193, 193, 143, 134, 255, 255, 145, 29, 284, 184, 269, 93, 16, 16, 267, 240, 37, 218, 6, 6, 2, 2, 146, 94s]
 	}
+
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s0e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T0h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T1e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T1h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T2e.png",
+	// //5
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T2h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/u10e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/u10h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/u6e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/u6h.png",
+	// //10
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r5e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r5h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r2e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/r2h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q4e.png",
+	// //15
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q4h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q1e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/q1h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s15e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s15h.png",
+	// //20
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s30e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/s30h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T3e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T3h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T4e.png",
+	// //25
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T4h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T5e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T5h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T6e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T6h.png",
+	// //30
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T7e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T7h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T8e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T8h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T9e.png",
+	// //35
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T9h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T10e.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T10h.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T10h.png",
+
+	//	'xstarts' : [65, 32, 35, 333, 95, 145, 35, 207, 172, 241, 46, 290, 210, 16, 158, 92, 51, 360, 325, 41, 55, 309, 317, 139, 278, 149, 284, 361, 359, 360, 305, 305, 67, 198, 52, 52, 35, 35],
+	//'ystarts' : [11, 59, 44, 94, 53, 113, 211, 211, 186, 173, 41, 183, 221, 285, 253, 283, 105, 8, 139, 139, 46, 45, 247, 31, 28, 256, 42, 55, 213, 238, 261, 261, 229, 276, 264, 264, 28, 172],
+	//'xends' :   [214, 298, 333, 35, 245, 95, 207, 35, 241, 172, 290, 46, 16, 210, 92, 158, 41, 325, 309, 55, 360, 51, 228, 218, 179, 247, 88, 164, 71, 71, 47, 47, 187, 317, 310, 310, 249, 249],
+	//'yends' :   [267, 243, 213, 262, 172, 232, 28, 28, 276, 265, 28, 169, 8, 72, 26, 26, 250, 152, 63, 63, 193, 193, 143, 134, 255, 28, 218, 231, 154, 178, 10, 10, 83, 130, 14, 14, 66, 211]
+//}
+
+
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T0e60.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T0e80.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/T0e100.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/ligne.png",
+	// "https://raw.githubusercontent.com/LiseBrun/mirror_trace/master/figures/ligne10.png"
 
 // 	'xstarts' : [32, 32, 32, 73, 86],
 // 	'ystarts' : [59, 59, 59, 228, 203],
